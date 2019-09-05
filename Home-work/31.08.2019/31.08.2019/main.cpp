5f556b24a1433e6a37f1de6f51b9e1dffa722f2e
павлюк назар <hasab.k18@gmail.com>
2019-09-06 01:10:00 +0300

    06.09.2019

--- Home-work/31.08.2019/31.08.2019/main.cpp
+++ Home-work/31.08.2019/31.08.2019/main.cpp
@@ -1,87 +1,275 @@
 #include<iostream>
 #include<string>
+#include<ctime>
+#include<queue>
 using namespace std;
 
-class Bullet
+enum Estimated_delivery_time{ Slow_delivery=1, Ordinary_delivery, Fast_delivery};
+struct Cargo;
+
+struct Cargo
 {
-	float caliber;
-	float weight;
-	float speed;
+private:
+	short weigth;
+	short size;
+	short ID_;
+	Estimated_delivery_time delivery_time;
 public:
-	virtual void Hit() = 0;
-	Bullet(float caliber, float weight, float speed)
+	static short Counter_id;
+	Cargo(short weigth,short size, Estimated_delivery_time delivery_time)
 	{
-		this->caliber = caliber;
-		this->weight = weight;
-		this->speed = speed;
+		ID_=++Counter_id;
+		this->weigth = weigth;
+		this->size = size;
+		this->delivery_time = delivery_time;
 	}
-	virtual void Start_move() = 0;
+	short Get_id()
+	{
+		return this->ID_;
+	}
+	short Get_size()
+	{
+		return this->size;
+	}
+	short Get_weigth()
+	{
+		return this->weigth;
+	}
+	Estimated_delivery_time Get_delivery_time()
+	{
+		return this->delivery_time;
+	}
 };
 
-class Shotgun_bullet :public Bullet
+class Transport
+{
+protected:
+	int carrying_capacity;
+	int luggage_compartment_size;
+	Estimated_delivery_time  speed_delivery;
+public:
+	virtual bool Add_Cargo(Cargo& cargo) = 0;
+};
+
+class Small_car_type_1:public Transport
 {
 public:
-	Shotgun_bullet(float caliber, float weight, float speed) :Bullet(caliber, weight, speed)
-	{}
-	void Hit()
+	Small_car_type_1()
 	{
-		cout << "Ai Ai Ai" << endl;
+		this->carrying_capacity = 500;
+		this->speed_delivery = Ordinary_delivery;
+		this->luggage_compartment_size = 500;
 	}
-	void Start_move()
+	virtual bool Add_Cargo(Cargo& cargo)override
 	{
-		cout << "Bang bang bang (Shotgun_bullet)" << endl;
+		if (carrying_capacity >= cargo.Get_weigth() && cargo.Get_delivery_time() == speed_delivery && luggage_compartment_size >= cargo.Get_size())
+		{
+			return true;
+		}
+		return false;
 	}
 };
-
-class Automatic_bullet :public Bullet
+class Small_car_type_2 :public Transport
 {
 public:
-	Automatic_bullet(float caliber, float weight, float speed) :Bullet(caliber, weight, speed)
-	{}
-	void Hit()
+	Small_car_type_2()
 	{
-		cout << "Ai Ai Ai" << endl;
+		this->carrying_capacity = 250;
+		this->speed_delivery = Fast_delivery;
+		this->luggage_compartment_size = 250;
 	}
-	void Start_move()
+	virtual bool Add_Cargo(Cargo& cargo)override
 	{
-		cout << "Bang bang bang (Automatic_bullet)" << endl;
+		if (carrying_capacity >= cargo.Get_weigth() && cargo.Get_delivery_time() == speed_delivery && luggage_compartment_size >= cargo.Get_size())
+		{
+			return true;
+		}
+		return false;
 	}
 };
 
 
-class Weapon
+class Minivan_car_type_1 :public Transport
 {
-	float caliber;
-	short damage;
-	double distance;
-	virtual Bullet* Create_bullet() = 0;
 public:
-	virtual Bullet* Shoot()
+	Minivan_car_type_1()
 	{
-		return Create_bullet();
+		this->carrying_capacity = 1000;
+		this->speed_delivery = Fast_delivery;
+		this->luggage_compartment_size = 1000;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity>= cargo.Get_weigth()&& carrying_capacity/2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size()&& luggage_compartment_size/2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+class Minivan_car_type_2 :public Transport
+{
+public:
+	Minivan_car_type_2()
+	{
+		this->carrying_capacity = 850;
+		this->speed_delivery = Ordinary_delivery;
+		this->luggage_compartment_size = 850;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
 	}
 };
 
-class Shotgun :public Weapon
+class Bus_car_type_1 :public Transport
 {
-	Bullet* Create_bullet()
+public:
+	Bus_car_type_1()
 	{
-		return new Shotgun_bullet(1, 2, 3);
+		this->carrying_capacity = 2000;
+		this->speed_delivery = Ordinary_delivery;
+		this->luggage_compartment_size = 2000;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
 	}
 };
-class Automatic :public Weapon
+class Bus_car_type_2 :public Transport
 {
-	Bullet* Create_bullet()
+public:
+	Bus_car_type_2()
 	{
-		return new Automatic_bullet(11, 22, 33);
+		this->carrying_capacity = 1500;
+		this->speed_delivery = Slow_delivery;
+		this->luggage_compartment_size = 1500;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
 	}
 };
 
+class Truck_car_type_1 :public Transport
+{
+public:
+	Truck_car_type_1()
+	{
+		this->carrying_capacity = 4000;
+		this->speed_delivery = Slow_delivery;
+		this->luggage_compartment_size = 4000;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+class Truck_car_type_2 :public Transport
+{
+public:
+	Truck_car_type_2()
+	{
+		this->carrying_capacity = 3500;
+		this->speed_delivery = Ordinary_delivery;
+		this->luggage_compartment_size = 3500;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+class Airplane_type_1 :public Transport
+{
+public:
+	Airplane_type_1()
+	{
+		this->carrying_capacity = 4000;
+		this->speed_delivery = Ordinary_delivery;
+		this->luggage_compartment_size = 4000;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+class Airplane_type_2 :public Transport
+{
+public:
+	Airplane_type_2()
+	{
+		this->carrying_capacity = 3500;
+		this->speed_delivery = Fast_delivery;
+		this->luggage_compartment_size = 3500;
+	}
+	virtual bool Add_Cargo(Cargo& cargo)override
+	{
+		if ((carrying_capacity >= cargo.Get_weigth() && carrying_capacity / 2 <= cargo.Get_weigth()) && cargo.Get_delivery_time() == speed_delivery && (luggage_compartment_size >= cargo.Get_size() && luggage_compartment_size / 2 >= cargo.Get_size()))
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+
+short Rand(short num)
+{
+	const short step = 5;
+	return (rand() % (num/step + 1)*step);
+}
+Estimated_delivery_time Rand_estimated_delivery_time()
+{
+	switch (Rand(3))
+	{
+	case Ordinary_delivery:
+		return Ordinary_delivery;
+		break;
+	case Fast_delivery:
+		return Fast_delivery;
+		break;
+	case Slow_delivery:
+		return Slow_delivery;
+		break;
+	}
+}
+
+
+short Cargo::Counter_id = 0;
 int main()
 {
-	Weapon *g = new Automatic;
-	Bullet* bullet = g->Shoot();
-	bullet->Hit();
+	srand(unsigned(time(NULL)));
+	queue<Cargo> queue_;
+	for (short i = 0; i < 100; i++)
+	{
+	queue_.push(Cargo(Rand(1000), Rand(500), Rand_estimated_delivery_time()));
+	cout << queue_.back().Get_id();
+	}
 
 	system("pause");
 	return 0;
